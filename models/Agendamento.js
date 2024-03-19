7ed4d8b7a1c2aaab2e71ccb2adcce75227cad7a4
vek03 <vektromboni@gmail.com>
2024-03-18 21:35:59 -0300

    Esqueleto Pronto

---
+++ models/Agendamento.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize');
+const sequelize = require(__dirname + '/models/sequelize');
+
+const Agendamento = sequelize.define('agendamentos',{
+    nome:{
+        type: Sequelize.STRING
+    },
+    endereco:{
+        type: Sequelize.STRING
+    },
+    bairro:{
+        type: Sequelize.STRING
+    },
+    cep:{
+        type: Sequelize.STRING
+    },
+    cidade:{
+        type: Sequelize.STRING
+    },
+    estado:{
+        type: Sequelize.STRING
+    }
+})
+
+Agendamento.sync()
+
+module.exports = Agendamento
